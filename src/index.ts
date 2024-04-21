9c52c97669a2bba1e056836e5f5f258a9fddb39b
noylacgdev <luis@noyaventures.com>
2024-04-21 00:31:55 -0600

    fix: connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
 import { SignManager } from './manager';
 
 export const signManagerUtility = SignManager;
-
-const clientSign = new signManagerUtility(
-  'postgresql://root:root1@localhost:5432/ForwolDev?sslmode=no-verify'
-);
-
-async function test(): Promise<void> {
-  const data = await clientSign.loadTables({ schema: 'public' });
-  console.log(data);
-}
-
-void test();
